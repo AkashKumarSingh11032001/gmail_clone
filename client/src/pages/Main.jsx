5d7f58972d8cad7a7e41d119a9b99c8a821c4ca8
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-27 14:09:12 +0530

    skeleton done

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Header from '../components/Header'
-
+import React from "react";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
 
 const Main = () => {
   return (
-    <Header />
-  )
-}
+    <>
+      <Header />
+      <SideBar />
+      <div>Display mail</div>
+    </>
+  );
+};
 
-export default Main
+export default Main;
