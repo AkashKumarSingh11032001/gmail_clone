40dedbbef3ad98b027ded399b1d6a975c5827574
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-27 15:42:59 +0530

    side bar content

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Drawer, styled } from "@mui/material";
+import SideBarContent from "./SideBarContent";
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <Drawer
+      anchor="left"
+      open={true}
+      hideBackdrop={true}
+      ModalProps={{
+        keepMounted: true,
+      }}
+      variant="persistent"
+      sx={{
+        "& .MuiDrawer-paper": {
+          width: 250,
+          borderRight: "none",
+          background: "#f5F5F5",
+          marginTop: "64px",
+          height: "calc(100vh - 64px)",
+        },
+      }}
+    >
+      <SideBarContent />
+    </Drawer>
+  );
+};
 
-export default SideBar
+export default SideBar;
