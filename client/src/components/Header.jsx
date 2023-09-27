a04cb80e5e469d51cd40722731d72b1be2b10110
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-27 14:36:04 +0530

    header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,63 @@
 import React from "react";
-import { AppBar, Toolbar, styled } from "@mui/material";
-import { Menu as MenuIcon } from "@mui/icons-material";
+import { AppBar, Box, InputBase, Toolbar, styled } from "@mui/material";
+import { Menu as MenuIcon, Search, Tune,HelpOutlineOutlined, SettingsOutlined,AppsOutlined, AccountCircleOutlined } from "@mui/icons-material";
 import { gmailLogo } from "../constants/constant";
 
 const StyledAppBar = styled(AppBar)`
-    background: #f5F5F5;
-    box-shadow: none;
+  background: #f5f5f5;
+  box-shadow: none;
 `;
+
+const SearchWrapper = styled(Box)`
+  background: #eaf1fb;
+  margin-left: 80px;
+  border-radius: 8px;
+  min-width: 690px;
+  max-width: 720px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  & > div {
+    width: 100%;
+    padding: 20px;
+  }
+`;
+
+const OptionsWrapper = styled(Box)`
+    width: 100%;
+    display: flex;
+    justify-content: end;
+    & > svg {
+        margin-left: 20px;
+    }
+`
 
 const Header = () => {
   return (
     <>
       <StyledAppBar position="static">
         <Toolbar>
-          <MenuIcon color="action"/>
-          <img src={gmailLogo} alt="logo" style={{ width: 110, marginLeft: 15 }} />
+          <MenuIcon color="action" />
+          <img
+            src={gmailLogo}
+            alt="logo"
+            style={{ width: 110, marginLeft: 15 }}
+          />
+          <SearchWrapper>
+            <Search color="action"/>
+            <InputBase placeholder="Search mail"/>
+            <Tune color="action"/>
+          </SearchWrapper>
+
+          <OptionsWrapper>
+            <HelpOutlineOutlined color="action"/>
+            <SettingsOutlined color="action" />
+            <AppsOutlined color="action" />
+            <AccountCircleOutlined color="action" />
+          </OptionsWrapper>
+          
         </Toolbar>
       </StyledAppBar>
     </>
