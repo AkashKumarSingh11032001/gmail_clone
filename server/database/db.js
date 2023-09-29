0bae76dc9e8b88337748f8d7964f48c4a97813a9
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-29 15:42:32 +0530

    axios API call

--- server/database/db.js
+++ server/database/db.js
@@ -1,21 +1,20 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
+const connection = async () => {
+  mongoose.set("strictQuery", true);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      //   dbName: "gmail_database",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-const connection = async () =>{
-    mongoose.set('strictQuery', true);
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-        //   dbName: "gmail_database",
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        })
-    
-        console.log('MongoDB connected')
-    } catch (error) {
-        console.log("Ahhh! we got an error: ",error.message);
-    }
-}
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("Ahhh! we got an error: ", error.message);
+  }
+};
 
 export default connection;
