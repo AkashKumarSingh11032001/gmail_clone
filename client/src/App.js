0102dccbbab7cb6e3d9bf3844f52b0210f6424a4
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-04 06:06:13 +0530

    init roouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import Main from "./pages/Main";
+import {
+  Navigate,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Main />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <Main />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
