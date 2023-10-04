631e2d4ca70e0b29d955cd042347f9052da090a1
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-04 07:13:12 +0530

    sidebar comp routes

--- client/src/components/SideBarContent.jsx
+++ client/src/components/SideBarContent.jsx
@@ -4,9 +4,9 @@ import { Button, List, ListItem, styled } from "@mui/material";
 import { CreateOutlined } from "@mui/icons-material";
 import { SIDEBAR_DATA } from "../config/sidebar.config.js";
 import { NavLink, useParams } from "react-router-dom";
-// import { routes } from "../routes/routes";
+import { routes } from "../routes/routes";
 import ComposeMail from "./ComposeMail.jsx";
-import { useState } from 'react';
+import { useState } from "react";
 
 const ComposeButton = styled(Button)`
   background: #c2e7ff;
@@ -18,31 +18,35 @@ const ComposeButton = styled(Button)`
 `;
 
 const Container = styled(Box)({
-    padding: 8,
-    '& > ul': {
-        padding: '10px 0 0 5px',
-        fontSize: 14,
-        fontWeight: 500,
-        cursor: 'pointer'
+  padding: 8,
+  "& > ul": {
+    padding: "10px 0 0 5px",
+    fontSize: 14,
+    fontWeight: 500,
+    cursor: "pointer",
+    "& > a":{
+      textDecoration: 'none',
+      color: 'inherit'
     },
-    '& > ul > li > svg': {
-            marginRight: 20,
-        }
-    }
-)
+  },
+  "& > ul > li > svg": {
+    marginRight: 20,
+  },
+});
 
 const SideBarContent = () => {
   const [openDialog, setOpenDialog] = useState(false);
 
-  const onComposeClick = () =>{
-    setOpenDialog(true);
-  }
-
   const { type } = useParams();
+
+  const onComposeClick = () => {
+    setOpenDialog(true);
+  };
+
   return (
     <Container>
       <Box>
-        <ComposeButton onClick={()=> onComposeClick()}>
+        <ComposeButton onClick={() => onComposeClick()}>
           <CreateOutlined style={{ marginRight: 10 }} />
           Compose
         </ComposeButton>
@@ -50,19 +54,21 @@ const SideBarContent = () => {
 
       <List>
         {SIDEBAR_DATA.map((data) => (
+          <NavLink key={data.name} to={`${routes.emails.path}/${data.name}`}>
             <ListItem
-            style={
-              type === data.name.toLowerCase()
-                ? {
-                    backgroundColor: "#d3e3fd",
-                    borderRadius: "0 16px 16px 0",
-                  }
-                : {}
-            }
-          >
-            <data.icon fontSize="small" />
-            {data.title}
-          </ListItem>
+              style={
+                type === data.name.toLowerCase()
+                  ? {
+                      backgroundColor: "#d3e3fd",
+                      borderRadius: "0 16px 16px 0",
+                    }
+                  : {}
+              }
+            >
+              <data.icon fontSize="small" />
+              {data.title}
+            </ListItem>
+          </NavLink>
         ))}
       </List>
       <ComposeMail openDialog={openDialog} setOpenDialog={setOpenDialog} />
