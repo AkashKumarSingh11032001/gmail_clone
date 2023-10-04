8aff5d1637f39be74d1aad0a502bd334457882a4
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-04 17:30:25 +0530

    draft select all

--- client/src/components/Emails.jsx
+++ client/src/components/Emails.jsx
@@ -3,27 +3,75 @@ import { useEffect, useState } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import useApi from "../hooks/useApi";
 import { API_URLS } from "../services/api.urls";
+import { Box, List, Checkbox } from "@mui/material";
+import Email from "./Email";
+import { DeleteOutline } from "@mui/icons-material";
 
 const Emails = () => {
   const { openDrawer } = useOutletContext();
   const { type } = useParams();
 
   const getEmailsService = useApi(API_URLS.getEmailFromType);
-  // const deleteEmailsService = useApi(API_URLS.deleteEmails);
-  // const moveEmailsToBin = useApi(API_URLS.moveEmailsToBin);
+  const deleteEmailsService = useApi(API_URLS.deleteEmails);
+  const moveEmailsToBin = useApi(API_URLS.moveEmailsToBin);
+  // const deleteEmailsService = "";
+  // const moveEmailsToBin = "";
 
   useEffect(() => {
     getEmailsService.call({}, type);
   }, [type]);
 
+  const [starredEmail, setStarredEmail] = useState(false);
+  const [selectedEmails, setSelectedEmails] = useState([]);
+
+  const selectAllEmails = (e) => {
+    if (e.target.checked) {
+      const emails = getEmailsService?.response?.map((email) => email._id);
+      setSelectedEmails(emails);
+    } else {
+      setSelectedEmails([]);
+    }
+  };
+
+  const deleteSelectedEmails = () => {
+    if (type === "bin") {
+      deleteEmailsService.call(selectedEmails);
+    } else {
+      moveEmailsToBin.call(selectedEmails);
+    }
+    setStarredEmail((prevState) => !prevState);
+  };
+
   return (
-    <div
+    <Box
       style={
-        openDrawer ? { marginLeft: 250, width: "100%" } : { width: "100%" }
+        openDrawer
+          ? { marginLeft: 250, width: "100%" }
+          : { width: "100%" }
       }
     >
-      Emails
-    </div>
+      <Box
+        style={{
+          padding: "20px 10px 0 10px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Checkbox size="small" onChange={(e) => selectAllEmails(e)} />
+        <DeleteOutline onClick={(e) => deleteSelectedEmails(e)} />
+      </Box>
+      <List>
+        {getEmailsService?.response?.map((email) => (
+          <Email
+            email={email}
+            key={email._id}
+            setStarredEmail={setStarredEmail}
+            selectedEmails={selectedEmails}
+            setSelectedEmails={setSelectedEmails}
+          />
+        ))}
+      </List>
+    </Box>
   );
 };
 
